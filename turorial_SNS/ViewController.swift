8ba1332f00139c191a17725412a5c9137aa8c2c7
Tatsuika <tatu3939104@gmail.com>
2018-10-10 01:39:55 +0900

    add_closure

--- turorial_SNS/ViewController.swift
+++ turorial_SNS/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func showActivityView(_ sender: UIBarButtonItem) {
         let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
-        self.present(controller, animated: true, completion: nil)
+        self.present(controller, animated: true, completion: {()->Void in print("表示完了")})
     }
     override func viewDidLoad() {
         super.viewDidLoad()
